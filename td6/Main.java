11b9eff21f2eccab914144bedf2fde5690a31a34
m27bay <mickael.ledenmat@gmail.com>
2020-04-10 19:23:08 +0200

    V2.0: add javadoc

---
+++ td6/Main.java
@@ -0,0 +1,11 @@
+/**
+  * Class Main
+  */
+public class Main {
+  public static void main(String[] args) {
+
+    SaisieRPN test = new SaisieRPN();
+    test.saisie();
+    // test.saisie2();
+  }
+}
